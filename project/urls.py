26118a5d296b3fb2d2130961047cbf7528b97e5f
codesshaman <nicksemail@yandex.ru>
2025-02-23 20:58:35 +0300

    add changed admin panel path

--- project/urls.py
+++ project/urls.py
@@ -14,12 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from dotenv import load_dotenv
 from django.contrib import admin
 from django.urls import path, include
 
+# Загружаем .env файл
+load_dotenv()
+
+# Получаем путь к админке
+admin_path = os.getenv('ALLOWED_ADMIN', 'admin')
+
+# Добавляем слеш, если его нет
+if not admin_path.endswith('/'):
+    admin_path += '/'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_path, admin.site.urls),
     path('api/', include('apps.api.urls')),
     path('mail/', include('apps.mail.urls')),
 ]
